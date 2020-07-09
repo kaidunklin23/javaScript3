cb031b81e64cd791ace5355081c853427088bc59
Kaija Dunklin <kaykay_s@yahoo.com>
2020-07-09 17:14:34 -0400

    7/9 hw

--- app.js
+++ app.js
@@ -43,3 +43,32 @@ flower.scent = "stinks";
 kangaroo["name"] = "karima";
 microphone["sound"] = "quiet";
 flower["color"] = "yellow";
+
+var camArray = [];
+
+for (var i = 1; i < 11; i++){
+    camArray.push(i);
+}
+console.log(camArray);
+
+var kefArray = [];
+var i = 10;
+while (i >= 0  ){
+    kefArray.push(i);
+    i--;
+}
+console.log(kefArray);
+
+var nikArray = [];
+for (var i = 11; i < 22; i += 2) {
+    nikArray.push(i);
+}
+console.log(nikArray);
+
+var wanArray = [];
+ var i = 15;
+while (i >= 0){
+wanArray.push(i);
+i--;
+}
+console.log(wanArray);
